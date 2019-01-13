6a089bb65c41078a5a0eb3ddac4f13407eac957d
Masen Matthews <masenmatthews@gmail.com>
2019-01-13 15:42:17 -0800

    initialize project with create-react-app and add inital components for forecast, form, and header

---
+++ src/components/Forecast.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Forecast = (props) => {
+  return(
+       <div>
+       </div>
+   )
+}
+export default Forecast;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) =>{
+  return (
+    <div>
+      <h1>Welcome to the Weather App</h1>
+      <p>Enter a location to get weather info now:</p>
+    </div>
+  )
+}
+export default Header;
