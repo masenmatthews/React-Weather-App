db2ce0a8055087814c2be176d7d1534d93b571fd
Masen Matthews <masenmatthews@gmail.com>
2019-01-14 15:39:36 -0800

    finish back end, moving on to styling

--- src/App.js
+++ src/App.js
@@ -6,22 +6,50 @@ import Forecast from './components/Forecast'
 const Api_Key = "ccadac3b1a40d8a243177e87aaac3c13";
 
 class App extends React.Component{
-  getWeather = async (e) => {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getForecast = async (e) => {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
     const response = await api_call.json()
     console.log(response);
-
+    if(city && country){
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      error: ""
+    })
+  }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
   }
 
    render(){
     return(
       <div>
         <Header />
-        <Form loadWeather={this.getWeather} />
-        <Forecast />
+        <Form loadWeather={this.getForecast} />
+        <Forecast
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error} />
       </div>
    )
   }
