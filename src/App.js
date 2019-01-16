fe27a3b196ba0e864f336701ce96d6f43ef39184
Masen Matthews <masenmatthews@gmail.com>
2019-01-16 15:47:37 -0800

    edit GET request to troubleshoot hosting problem

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component{
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
     const response = await api_call.json()
     console.log(response);
     if(city && country){
